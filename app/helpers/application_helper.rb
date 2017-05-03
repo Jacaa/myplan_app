aee8fe6894d14f478691d2b0264858b1520d8e8f
Jacek <mikolajczak.jaca@gmail.com>
2017-05-03 17:14:26 +0200

    Add static pages, improve layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(page_title = '')
+    base_title = 'MyPlan'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
